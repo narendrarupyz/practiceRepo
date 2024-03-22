7e6e1b6ef6a5e77f9e58bb87edce6f54489f55a3
narendrarupyz <narendra.kumar@rupyz.com>
2024-03-22 23:46:15 +0530

    forth commit

---
+++ SetOperation.py
@@ -0,0 +1,9 @@
+#creation of set of String
+Colors = {'red','blue','green'}
+Colors
+#{'green','red','blue'} output will be always unorder it may vary
+
+#Duplicate value is not allowed in set simply it will ignore duplicate item
+Colors = {'green','red','blue','red','yellow'}
+print(Colors)
+#{'green','red','blue','yellow'}
